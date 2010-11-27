c68f06ddd92dbcc2fde10bdf889e463d0b4138c1
shurik <aleksandr@butchershopcreative.com>
2010-11-27 00:33:57 -0800

    add map and geo-location

--- src/places/places/static/js/master.js
+++ src/places/places/static/js/master.js
@@ -1,8 +1,20 @@
 $(function() {
+  var browserSupportFlag;
   var latlng = new google.maps.LatLng(-34.397, 150.644);
   var map = new google.maps.Map($('#map')[0], {
     zoom: 8,
     center: latlng,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
+  if(navigator.geolocation) {
+    $.jGrowl("Locating...");
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      $.jGrowl("Found your location");
+      map.setCenter(initialLocation);
+    }, function() {
+      $.jGrowl("Your browser doesn't support GeoLocation");
+    });
+  }
 })
